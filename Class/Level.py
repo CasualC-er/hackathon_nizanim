89ae88db21d3d90bbb75fc4be03ab45d1adf0f6f
Ariel <ariel.petrover@gmail.com>
2023-03-15 19:02:05 +0200

    changed block size to 64 added basic draw to Level.py

--- Class/Level.py
+++ Class/Level.py
@@ -1,3 +1,7 @@
+import pygame
+from constants import *
+
+
 class Level:
 
     def __init__(self, field: list):
@@ -8,10 +12,17 @@ class Level:
         dic = {}
         for layer in range(len(self.field)):
             for tile in range(len(self.field[layer])):
-                if self.field[layer][tile] != ' ':
-                    pos = (tile * 16, layer * 16)
+                tile_char = self.field[layer][tile]
+                if tile_char != ' ':
+                    pos = (tile * BLOCK_SIZE, layer * BLOCK_SIZE)
                     dic[pos] = self.field[layer][tile]
         return dic
 
-    def create_field(self):
+    def create_field(self, screen):
+        for key in self.field_dictionary:
+            x = key[0]
+            y = key[1]
 
+            if self.field_dictionary[key] == FLOOR:
+                square = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+                pygame.draw.rect(screen, (90, 50, 10), square)
