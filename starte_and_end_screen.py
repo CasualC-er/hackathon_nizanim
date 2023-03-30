155379ea28f00919f988a07d4c1a5750e2188fc1
Laly Brami <lalybrami@gmail.com>
2023-03-30 12:44:19 +0300

    added statr screen

---
+++ starte_and_end_screen.py
@@ -0,0 +1,32 @@
+import pygame
+from constants import *
+
+
+def starte_screen():
+    pygame.init()
+    screen_size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+    screen = pygame.display.set_mode(screen_size)
+    pygame.display.set_caption('Jumping Lad')
+    finish = False
+
+    while not finish:
+
+        font = pygame.font.SysFont('Ariel', 60)
+        welcome_text = font.render('WELCOME TO JUMPING LAD', True, TEXT_COLOR)
+        screen.blit(welcome_text, [20, 20])
+        play_text = font.render('PLAY', True, TEXT_COLOR)
+        screen.blit(play_text, [20, 60])
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finish = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                if 20 < pos[0] < 120 and 60 < pos[1] < 90:
+                    return True
+
+        pygame.display.flip()
+    pygame.quit()
+
+
+print(starte_screen())
